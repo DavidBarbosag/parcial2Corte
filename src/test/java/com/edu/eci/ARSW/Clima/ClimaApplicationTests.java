d6c1f760927d280596f1df26627b63226a55107c
unknown <david.barbosa-g@labinfo.is.escuelaing.edu.co>
2025-07-22 08:57:20 -0500

    primer commit

---
+++ src/test/java/com/edu/eci/ARSW/Clima/ClimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.eci.ARSW.Clima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
